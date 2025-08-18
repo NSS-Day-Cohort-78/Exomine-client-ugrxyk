35d39010a4ebee0e7c7a030f78204fceca4a1a73
McKenney Leavell <maleavell@gmail.com>
2025-08-18 15:09:16 -0500

    updating inventory module

--- scripts/inventory.js
+++ scripts/inventory.js
@@ -1,4 +1,17 @@
 // fetch colony inventory
-const colonyInventory = async () => {
-    const governor = await fetch("http://localhost:8088/colonyInventories").then(res => res.json())
+export const colonyInventory = async (event) => {
+    const inventory = await fetch("http://localhost:8088/colonyInventories?_expand=mineral").then(res => res.json())
+
+    if (event.target.id === "governors") {
+        for (const colonyInventory of inventory) {
+            if (colonyId === event.target.dataset.colonyId)
+        }
+    }
+
 }
+
+
+// if governor is selected
+    // loop colonyInventories[]
+        // if array colonyId === governor.colonyId...
+            // add to HTML string: [x] tons of [mineral]
