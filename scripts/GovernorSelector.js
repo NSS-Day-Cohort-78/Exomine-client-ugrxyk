2329635bb4ed7b80e2cb33de6e6e13f515a2126b
charlesmerriman <zacmerr@gmail.com>
2025-08-18 15:16:00 -0500

    Finished GovernorSelector for real this time

--- scripts/GovernorSelector.js
+++ scripts/GovernorSelector.js
@@ -1,10 +1,18 @@
 import { getGovernors } from "./service/GovernorService.js";
-import { setGovernor } from "./TransientState.js";
+import { setFacility, setGovernor } from "./TransientState.js";
+import { getFacilities } from "./service/FacilityService.js";
 
-const changeHandler = (changeEvent) => {
+const changeHandlerGovernor = (changeEvent) => {
   if (changeEvent.target.value === "governor") {
     const chosenOption = changeEvent.target.id;
     setGovernor(chosenOption);
+  }
+};
+
+const changeHandlerFacility = (changeEvent) => {
+  if (changeEvent.target.value === "facility") {
+    const chosenOption = changeEvent.target.id;
+    setFacility(chosenOption);
   }
 };
 
@@ -14,13 +22,32 @@ export const governorDropDown = async () => {
     (governor) => governor.status === true
   );
 
-  document.addEventListener("change", changeHandler);
+  document.addEventListener("change", changeHandlerGovernor);
 
   let dropDownHTML = `<label for="governors">Choose a governor</label>
       <select id="governors" name="governor_selector">`;
 
   filteredGovernors.map((governor) => {
     dropDownHTML += `<option data-id=${governor.id} data-colonyId="${governor.colony.id}" value="governor">${governor.name}</option>`;
+  });
+
+  dropDownHTML += `</select>`;
+  return dropDownHTML;
+};
+
+export const facilityDropDown = async () => {
+  const facilityArray = await getFacilities();
+  const filteredFacilities = await facilityArray.filter(
+    (facility) => facility.status === true
+  );
+
+  document.addEventListener("change", changeHandlerFacility);
+
+  let dropDownHTML = `<label for="facilities">Choose a facility</label>
+      <select id="facilities" name="facility_selector">`;
+
+  filteredFacilities.map((facility) => {
+    dropDownHTML += `<option data-id=${facility.id} value="facility">${facility.name}</option>`;
   });
 
   dropDownHTML += `</select>`;
