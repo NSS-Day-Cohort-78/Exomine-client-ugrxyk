e2c304eaaff0e861f36b8de006148a58baada2c2
McKenney Leavell <maleavell@gmail.com>
2025-08-18 14:22:27 -0500

    add inventory module and fetch from database

--- scripts/inventory.js
+++ scripts/inventory.js
@@ -1,8 +1,8 @@
 // fetch governors and colony inventory
 const governorsList = async () => {
-    const governor = await fetch("").then(res => res.json())
+    const governor = await fetch("http://localhost:8088/governors").then(res => res.json())
 }
 
 const colonyInventory = async () => {
-    const governor = await fetch("").then(res => res.json())
+    const governor = await fetch("http://localhost:8088/colonyInventories").then(res => res.json())
 }
