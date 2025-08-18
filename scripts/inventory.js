28a241ec37cac4dae956b0f7c10ef531344b265d
McKenney Leavell <maleavell@gmail.com>
2025-08-18 14:13:51 -0500

    create inventory module

---
+++ scripts/inventory.js
@@ -0,0 +1,11 @@
+// fetch governors and colony inventory
+const governorsList = async () => {
+    const governor = await fetch("").then(res => res.json())
+}
+
+const governorsList = async () => {
+    const governor = await fetch("").then(res => res.json())
+}
+
+// if governor is selected, get their colonyId
+    // if colony ID matches colonyId, display mineral inventory
