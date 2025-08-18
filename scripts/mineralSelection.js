932f79d1a95ae56d1220782fbf21bc7f4d17c3da
Evan Alford <evan.al4d@gmail.com>
2025-08-18 15:01:38 -0500

    working mineralSelection

---
+++ scripts/mineralSelection.js
@@ -0,0 +1,9 @@
+//import database 
+import {database} from "./api/database.json"
+//create html to show minerals (mineralsHTML) for specific facility
+const mineralSelection = () => {
+    let mineralsHTML = ''
+    
+}
+]
+//return mineralsHTML
