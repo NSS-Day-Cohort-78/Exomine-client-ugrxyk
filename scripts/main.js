d8161f550b7826ae34b5f408b10b2dbc72bc4394
McKenney Leavell <maleavell@gmail.com>
2025-08-18 13:13:42 -0500

    updated main.js and index.html

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+
+const container = document.querySelector("#container")
+
+const render = async () => {
+    const html = `
+        <h1>Solar System Mining Marketplace</h1>
+
+        <article class="drop-downs">
+        </article>
+
+        <article class="facilityMineralsChoice">
+        </article>
+
+        <article class="colonyMineralsInventory">
+        </article>
+
+        <article class="spaceCart">
+            <h2>Space Cart</h2>
+        </article>
+    `
+    container.innerHTML = html
+}
+
+render()
