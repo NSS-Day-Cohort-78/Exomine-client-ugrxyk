bd57ecc09e5c48dbb936794e941f2aa37d078e0f
charlesmerriman <zacmerr@gmail.com>
2025-08-19 10:27:27 -0500

    Added bootstrap classes to main.js

--- scripts/main.js
+++ scripts/main.js
@@ -8,20 +8,26 @@ const render = () => {
   const html = `
         <h1>Solar System Mining Marketplace</h1>
 
-        <article class="drop-downs">
-            ${governorDropDownHTML}
-            ${facilityDropDownHTML}
-        </article>
+        <div class="container-fluid">
+          <div class="row">
+            <article class="col dropdown">
+              ${governorDropDownHTML}
+              ${facilityDropDownHTML}
+            </article>
 
-        <article class="facilityMineralsChoice">
-        </article>
+            <article class="col colonyInventory">
+            </article>
+          </div>
 
-        <article class="colonyMineralsInventory">
-        </article>
-
-        <article class="spaceCart">
-            <h2>Space Cart</h2>
-        </article>
+          <div class="row">
+            <article class="colonyMineralsInventory col">
+            </article>
+            
+            <article class="spaceCart col">
+              <h2>Space Cart</h2>
+            </article>
+          </div>
+        </div>
     `;
   container.innerHTML = html;
 };
