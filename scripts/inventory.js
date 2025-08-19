30ec3b5e1de390cd3f94a1c7b94efbfdb72d8ccb
McKenney Leavell <maleavell@gmail.com>
2025-08-19 09:33:01 -0500

    save

--- scripts/inventory.js
+++ scripts/inventory.js
@@ -1,22 +1,20 @@
-// fetch colony inventory
+
 export const colonyInventory = async (event) => {
     const inventory = await fetch("http://localhost:8088/colonyInventories?_expand=mineral").then(res => res.json())
 
     let html = ""
 
-    if (event.target.id === "governors") {
+    // if (event.target.id === "governors") {
         for (const colonyInventory of inventory) {
-            if (colonyInventory.colonyId === event.dataset.colonyId) {
+            if (parseInt(event.dataset.colonyId) === colonyInventory.colonyId) {
                 html += `${colonyInventory.quantity} tons of ${colonyInventory.mineral.name}`
             }
         }
-    }
+    // }
 
     return html
 
 }
-
-
 
 // if governor is selected
     // loop colonyInventories[]
