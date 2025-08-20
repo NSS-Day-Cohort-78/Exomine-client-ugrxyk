9dc81649982c06106c875b5563a01c9b7742397e
Evan Alford <evan.al4d@gmail.com>
2025-08-20 10:22:17 -0500

    maybe completed mineralSelection

--- scripts/mineralSelection.js
+++ scripts/mineralSelection.js
@@ -1,24 +1,32 @@
-//import database 
-import {database} from "./api/database.json"
-//create html to show minerals (mineralsHTML) for specific facility
- export const mineralSelection = (facilityId) => {
-    //find the selected facility
-    const selectedFacility = database.miningFacilities.find(facility => facility.id === facilityId)
-    //get all minerals available at this facility
-    const facilityMinerals = database.facilityMinerals.filter(facilityMineral => facilityMineral.miningFacilityId === facilityId
-    )};
+//Export and declare GetMineralSelection, fetch database 
+export const GetMineralSelection = async (facilityId) => {
+  const response = await fetch("./api/database.json");
+  const database = await response.json();
+  //find Selected facility
+  const selectedFacility = database.miningFacilities.find(
+    (facility) => facility.id === facilityId
+  );
+  // Get minerals for this specific facility only
+  const facilityMinerals = database.facilityMinerals.filter(
+    (fm) => fm.miningFacilityId === facilityId
+  );
 
-    let mineralsHTML = 
-    `<div class="mineral-selection">
-    <h2> Facility minerals for ${selectedFacility ? selectedFacility.name : 'unknown Facility'}</h2>
-    <div class="minerals-list">`
-//loop through each mineral at this facility
-for (const facilityMineral of facilityMinerals) {
+  let mineralsHTML = `
+    <div class="mineral-selection">
+    <h2> Facility minerals for ${
+      selectedFacility ? selectedFacility.name : "unknown Facility"
+    }</h2>
+    <div class="minerals-list">
+    `;
+  //loop through each mineral at this facility
+  for (const facilityMineral of facilityMinerals) {
     //find mineral details
-    const mineral = database.minerals.find(minerals => minerals.id === facilityMineral.mineralID)
-    //only create radio button if quanity > 0 
+    const mineral = database.minerals.find(
+      (minerals) => minerals.id === facilityMineral.mineralId
+    );
+    //only create radio button if quanity > 0
     if (facilityMineral.quanity > 0) {
-        mineralsHTML += `
+      mineralsHTML += `
         <div class= "mineral-option">
         <input type ="radio"
                 name= "mineral"
@@ -28,30 +36,22 @@ for (const facilityMineral of facilityMinerals) {
                 ${mineral.name} (${facilityMineral.quanity} tons available)
             </label>
             </div>
-            `
-            //show mineral but without button if quantity is 0
+            `;
+      //show mineral but without button if quantity is 0
     } else {
-        mineralsHTML += `
-        <div class = mineral-option mineral-unavailable">
+      mineralsHTML += `
+        <div class ="mineral-option mineral-unavailable">
         <span class="unavailable-mineral">
             ${mineral.name} (Out of stock)
             </span> 
-        </div>`
+        </div>`;
     }
-}
+  }
 
-mineralsHTML += `
+  mineralsHTML += `
         </div>
     </div>
-`
-//return mineralsHTML
-return mineralsHTML
-    
-
-    
-    
-    
-    
-    
-
-
+`;
+  //return mineralsHTML
+  return mineralsHTML;
+};
